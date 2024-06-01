fecb336a93c3cc74bd3ad1831cfa9d60db2b3c6d
nomanluhar <luharnoman409@gmail.com>
2024-06-01 15:00:15 +0530

    Create methods added

--- routes/employee/create.js
+++ routes/employee/create.js
@@ -1,12 +1,59 @@
 const express = require("express");
 const router = express.Router();
-// const User = require("../models/Employee");
+const Employee = require("../../models/Employee");
+const employeeData = require("../../employee-data.json"); // Path to your JSON file
 
 //Add employee
-router.post("/", (req, res) => {
-  console.log("Employee create function call-");
+router.post("/", async (req, res) => {
+  try {
+    // Check if an employee with the given email already exists
+    const existingEmployee = await Employee.findOne({ email: req.body.email });
+    if (existingEmployee) {
+      return res
+        .status(400)
+        .json({ error: "Employee with this email already exists" });
+    }
 
-  res.status(200).json("Created successfully");
+    const user = new Employee(req.body);
+    await user.save();
+    res.status(201).json("Created successfully");
+  } catch (error) {
+    console.error({ error });
+    res.status(500).json({ error: "Employee creation failed" });
+  }
+});
+
+router.post("/bulk", async (req, res) => {
+  try {
+    // Find existing employees with the same emails
+    const existingEmployees = await Employee.find({
+      email: { $in: employeeData.map((employee) => employee.email) },
+    });
+
+    // Extract existing email addresses
+    const existingEmails = existingEmployees.map((employee) => employee.email);
+
+    // Filter out employees with duplicate emails
+    const newData = employeeData.filter(
+      (employee) => !existingEmails.includes(employee.email)
+    );
+    const duplicates = employeeData.filter((employee) =>
+      existingEmails.includes(employee.email)
+    );
+
+    if (newData.length > 0) {
+      // Insert only new data into the database
+      await Employee.insertMany(newData);
+    }
+
+    res.status(201).json({
+      message: "Import operation completed",
+      duplicates: duplicates,
+    });
+  } catch (error) {
+    console.error({ error });
+    res.status(500).json({ error: "Employee creation failed" });
+  }
 });
 
 module.exports = router;
